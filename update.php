4a89e815e1ba8042a24f1b402bda55d283586c12
swarzchild <xx.son.xxtana@gmail.com>
2024-11-19 10:56:22 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,55 +1,39 @@
-<?php //ไฟล์นี้สำหรับเปลี่ยนแปลงหรืออัพเดทข้อมูลในตาราง letter
+<?php
 include('config.php');
-$id = $_GET['id'];
-$Fnamesend = $_GET['Fnamesend'];
-$Lnamesend = $_GET['Lnamesend'];
-$Pnumsend = $_GET['Pnumsend'];
-$Fnamereci = $_GET['Fnamereci'];
-$Lnamereci = $_GET['Lnamereci'];
-$Pnumreci = $_GET['Pnumreci'];
-$cityreci = $_GET['cityreci'];
-$UMreci = $_GET['UMreci'];
-$TUMreci = $_GET['TUMreci'];
-$Hnumreci = $_GET['Hnumreci'];
-$Preci = $_GET['Preci'];
-$TUreci = $_GET['TUreci'];
-$date = $_GET['date'];
 
-$sql = "UPDATE letter SET 
-            Fnamesend = :Fnamesend, 
-            Lnamesend = :Lnamesend, 
-            Pnumsend = :Pnumsend, 
-            Fnamereci = :Fnamereci, 
-            Lnamereci = :Lnamereci, 
-            Pnumreci = :Pnumreci, 
-            cityreci = :cityreci, 
-            UMreci = :UMreci, 
-            TUMreci = :TUMreci, 
-            Hnumreci = :Hnumreci, 
-            Preci = :Preci, 
-            TUreci = :TUreci, 
-            date = :date 
-        WHERE id = :id";
-$query = $dbcon->prepare($sql);
-$query->bindParam(':Fnamesend', $Fnamesend, PDO::PARAM_STR);
-$query->bindParam(':Lnamesend', $Lnamesend, PDO::PARAM_STR);
-$query->bindParam(':Pnumsend', $Pnumsend, PDO::PARAM_STR);
-$query->bindParam(':Fnamereci', $Fnamereci, PDO::PARAM_STR);
-$query->bindParam(':Lnamereci', $Lnamereci, PDO::PARAM_STR);
-$query->bindParam(':Pnumreci', $Pnumreci, PDO::PARAM_STR);
-$query->bindParam(':cityreci', $cityreci, PDO::PARAM_STR);
-$query->bindParam(':UMreci', $UMreci, PDO::PARAM_STR);
-$query->bindParam(':TUMreci', $TUMreci, PDO::PARAM_STR);
-$query->bindParam(':Hnumreci', $Hnumreci, PDO::PARAM_STR);
-$query->bindParam(':Preci', $Preci, PDO::PARAM_STR);
-$query->bindParam(':TUreci', $TUreci, PDO::PARAM_STR);
-$query->bindParam(':date', $date, PDO::PARAM_STR);
-$query->bindParam(':id', $id, PDO::PARAM_INT);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $id = $_POST['id'];
+    $Sname = $_POST['Sname'];
+    $Rname = $_POST['Rname'];
+    $PS = $_POST['PS'];
+    $PR = $_POST['PR'];
+    $city = $_POST['city'];
+    $subcity = $_POST['subcity'];
+    $subsubcity = $_POST['subsubcity'];
+    $Hnumber = $_POST['Hnumber'];
+    $TU = $_POST['TU'];
+    $Pcode = $_POST['Pcode'];
+    $date = $_POST['date'];
 
-$query->execute();
-if ($query->rowCount() > 0) {
-    echo "<script>alert('แก้ไขข้อมูลเรียบร้อย'); window.location='showdata_letter.php';</script>";
-} else {
-    echo "<script>alert('มีบางอย่างผิดพลาด'); window.location='showdata_letter.php';</script>";
+    $sql = "UPDATE letter SET Sname = :Sname, Rname = :Rname, PS = :PS, PR = :PR, city = :city, subcity = :subcity, subsubcity = :subsubcity, Hnumber = :Hnumber, TU = :TU, Pcode = :Pcode, date = :date WHERE id = :id";
+    $query = $dbcon->prepare($sql);
+    $query->bindParam(':id', $id, PDO::PARAM_INT);
+    $query->bindParam(':Sname', $Sname);
+    $query->bindParam(':Rname', $Rname);
+    $query->bindParam(':PS', $PS, PDO::PARAM_INT);
+    $query->bindParam(':PR', $PR, PDO::PARAM_INT);
+    $query->bindParam(':city', $city);
+    $query->bindParam(':subcity', $subcity);
+    $query->bindParam(':subsubcity', $subsubcity);
+    $query->bindParam(':Hnumber', $Hnumber, PDO::PARAM_INT);
+    $query->bindParam(':TU', $TU);
+    $query->bindParam(':Pcode', $Pcode, PDO::PARAM_INT);
+    $query->bindParam(':date', $date);
+
+    if ($query->execute()) {
+        echo "<script>alert('Updated successfully'); window.location='showdata_letter.php';</script>";
+    } else {
+        echo "<script>alert('Failed to update data'); window.location='showdata_letter.php';</script>";
+    }
 }
 ?>
